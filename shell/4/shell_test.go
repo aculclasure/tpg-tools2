51163c2df37f6ee32ba2ead9282736b254869fdd
Andrew Culclasure <elskohder@gmail.com>
2023-12-18 21:25:59 -0500

    CH08: Add transcript functionality to shell package for end-of-chapter exercise

---
+++ shell/4/shell_test.go
@@ -0,0 +1,77 @@
+package shell_test
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/aculclasure/shell"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestCmdFromString_CreatesExpectedCmd(t *testing.T) {
+	t.Parallel()
+	input := "/bin/ls -l"
+	want := []string{"/bin/ls", "-l"}
+	cmd, err := shell.CmdFromString(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := cmd.Args
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestCmdFromString_ErrorsOnEmptyInput(t *testing.T) {
+	t.Parallel()
+	_, err := shell.CmdFromString("")
+	if err == nil {
+		t.Fatal("want error on empty input but got nil")
+	}
+}
+
+func TestNewSession_CreatesExpectedSession(t *testing.T) {
+	t.Parallel()
+	want := shell.Session{
+		Stdin:      os.Stdin,
+		Stdout:     os.Stdout,
+		Stderr:     os.Stderr,
+		Transcript: io.Discard,
+	}
+	got := *shell.NewSession(os.Stdin, os.Stdout, os.Stderr)
+	if want != got {
+		t.Errorf("want %#v, got %#v", want, got)
+	}
+}
+
+func TestRunProducesExpectedOutput(t *testing.T) {
+	t.Parallel()
+	in := strings.NewReader("echo hello\n\n")
+	out := new(bytes.Buffer)
+	session := shell.NewSession(in, out, io.Discard)
+	session.DryRun = true
+	session.Run()
+	want := "> echo hello\n> > \nBe seeing you!\n"
+	got := out.String()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestRunProducesExpectedTranscript(t *testing.T) {
+	t.Parallel()
+	in := strings.NewReader("echo hello\n\n")
+	transcript := new(bytes.Buffer)
+	session := shell.NewSession(in, io.Discard, io.Discard)
+	session.DryRun = true
+	session.Transcript = transcript
+	session.Run()
+	want := "> echo hello\necho hello\n> \n> \nBe seeing you!\n"
+	got := transcript.String()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
