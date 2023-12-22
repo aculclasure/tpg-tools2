d0b1d2bd8adb5e5d67e71cc7c1f69778c54a1720
Andrew Culclasure <elskohder@gmail.com>
2023-12-21 19:58:04 -0500

    CH09: Add loc package and command as end-of-chapter exercise

---
+++ loc/1/cmd/loc/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/bitfield/script"
+)
+
+func main() {
+	lines, err := script.FindFiles(".").
+		MatchRegexp(regexp.MustCompile(`.go$`)).
+		Concat().
+		RejectRegexp(regexp.MustCompile(`^$`)).
+		CountLines()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Printf("You've written %d lines of Go in this project!\n", lines)
+}
