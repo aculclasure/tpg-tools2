85ed9b2842023294468af884787d4a7cec4c1d0b
Andrew Culclasure <elskohder@gmail.com>
2023-12-22 11:05:38 -0500

    CH10: Update kv package

--- kv/1/kv.go
+++ kv/1/kv.go
@@ -1,13 +1,35 @@
 package kv
 
+import (
+	"encoding/gob"
+	"errors"
+	"io/fs"
+	"os"
+)
+
 type store struct {
+	path string
 	data map[string]string
 }
 
 func OpenStore(path string) (*store, error) {
-	return &store{
+	s := &store{
+		path: path,
 		data: map[string]string{},
-	}, nil
+	}
+	f, err := os.Open(path)
+	if errors.Is(err, fs.ErrNotExist) {
+		return s, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	err = gob.NewDecoder(f).Decode(&s.data)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
 }
 
 func (s store) Get(key string) (string, bool) {
@@ -17,4 +39,13 @@ func (s store) Get(key string) (string, bool) {
 
 func (s *store) Set(key, value string) {
 	s.data[key] = value
+}
+
+func (s store) Save() error {
+	f, err := os.Create(s.path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return gob.NewEncoder(f).Encode(&s.data)
 }
--- kv/1/kv_test.go
+++ kv/1/kv_test.go
@@ -1,6 +1,7 @@
 package kv_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/aculclasure/kv"
@@ -48,5 +49,69 @@ func TestSetUpdatesExistingKeyToNewValue(t *testing.T) {
 	}
 	if v != "updatedValue" {
 		t.Errorf("want 'updatedValue', got %s", v)
+	}
+}
+
+func TestSaveSavesDataPersistently(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/test.store"
+	store1, err := kv.OpenStore(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	store1.Set("key1", "value1")
+	store1.Set("key2", "value2")
+	store1.Set("key3", "value3")
+	err = store1.Save()
+	if err != nil {
+		t.Fatal(err)
+	}
+	store2, err := kv.OpenStore(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v, _ := store2.Get("key1"); v != "value1" {
+		t.Errorf("want key1 to have value 'value1', got %s", v)
+	}
+	if v, _ := store2.Get("key2"); v != "value2" {
+		t.Errorf("want key2 to have value 'value2', got %s", v)
+	}
+	if v, _ := store2.Get("key3"); v != "value3" {
+		t.Errorf("want key3 to have value 'value3', got %s", v)
+	}
+}
+
+func TestSaveErrorsOnUnwritablePath(t *testing.T) {
+	t.Parallel()
+	s, err := kv.OpenStore("fakedir/unwritable.store")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.Save()
+	if err == nil {
+		t.Fatal("no error")
+	}
+}
+
+func TestOpenStore_ReturnsErrorForUnreadablePath(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/unreadable.store"
+	if _, err := os.Create(path); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Chmod(path, 0000); err != nil {
+		t.Fatal(err)
+	}
+	_, err := kv.OpenStore(path)
+	if err == nil {
+		t.Fatal("no error")
+	}
+}
+
+func TestOpenStore_ReturnsErrorOnInvalidData(t *testing.T) {
+	t.Parallel()
+	_, err := kv.OpenStore("testdata/empty.store")
+	if err == nil {
+		t.Fatal("no error")
 	}
 }
