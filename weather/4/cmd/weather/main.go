9efe70c66a49abf0fc6161eb0ec9ae5e6ffda837
Andrew Culclasure <elskohder@gmail.com>
2023-12-23 11:19:42 -0500

    CH11: Add weather client

---
+++ weather/4/cmd/weather/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aculclasure/weather"
+)
+
+const Usage = `Usage: weather LOCATION
+
+Example: weather London,UK`
+
+func main() {
+	apiKey := os.Getenv("OPENWEATHERMAP_API_KEY")
+	if apiKey == "" {
+		fmt.Fprintln(os.Stderr, "Please set the environment variable OPENWEATHER_API_KEY.")
+		os.Exit(1)
+	}
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, Usage)
+		os.Exit(0)
+	}
+	location := os.Args[1]
+	c := weather.NewClient(apiKey)
+	conditions, err := c.GetWeather(location)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(conditions)
+}
