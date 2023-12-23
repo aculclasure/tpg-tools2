fe5379be23d48221ce2bb8dde8466875891b860a
Andrew Culclasure <elskohder@gmail.com>
2023-12-23 08:48:44 -0500

    CH11: Add ParseData() function in weather package

--- weather/2/weather.go
+++ weather/2/weather.go
@@ -1,7 +1,32 @@
 package weather
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Conditions struct {
 	Summary string
 }
 
-func ParseData(data []byte) {}
+type OWMResponse struct {
+	Weather []struct {
+		Main string
+	}
+}
+
+func ParseData(data []byte) (Conditions, error) {
+	var resp OWMResponse
+	err := json.Unmarshal(data, &resp)
+	if err != nil {
+		return Conditions{}, fmt.Errorf(
+			"got error '%s' trying to parse openweather response: %s", err, string(data))
+	}
+	if len(resp.Weather) < 1 {
+		return Conditions{}, fmt.Errorf(
+			"got error '%s' trying to parse openweather response: %s, want at least one weather element", err, string(data))
+	}
+	return Conditions{
+		Summary: resp.Weather[0].Main,
+	}, nil
+}
--- weather/2/weather_test.go
+++ weather/2/weather_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/aculclasure/weather"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -22,5 +23,25 @@ func TestParseData_CorrectlyParsesJSONData(t *testing.T) {
 	}
 	if !cmp.Equal(want, got) {
 		cmp.Diff(want, got)
+	}
+}
+
+func TestParseData_ReturnsErrorOnEmptyData(t *testing.T) {
+	t.Parallel()
+	_, err := weather.ParseData([]byte{})
+	if err == nil {
+		t.Fatal("want error parsing empty response, got nil")
+	}
+}
+
+func TestParseData_ReturnsErrorOnIncompleteJSONData(t *testing.T) {
+	t.Parallel()
+	data, err := os.ReadFile("testdata/incomplete_weather_response.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = weather.ParseData(data)
+	if err == nil {
+		t.Fatal("want error parsing incomplete response, got nil")
 	}
 }
